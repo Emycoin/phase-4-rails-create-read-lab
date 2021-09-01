e6cdf2f088f75ff5afdde1c56c410ee2f7eeec02
Emycoin <emytausevich@gmail.com>
2021-08-31 20:55:25 -0400

    completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,29 @@
+class PlantsController < ApplicationController
+    #Get
+    def index
+        plants = Plant.all 
+        render json: plants
+    end
+
+    #Show
+    def show 
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: {error: 'Plant not found'}, status: :not_found
+        end
+    end
+
+
+    #Create
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+    private
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
